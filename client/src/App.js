5bab5e9491633daf8c5750a3d504c8e741dd6b22
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-10-03 01:09:25 -0500

    added component for poem form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
 
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import PoemsContainer from './PoemsContainer';
 import Navigation from './Navigation';
+import PoemPost from './PoemPost';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
       <div className="App">
         <Navigation/>
         <Routes>
+          <Route path="/poem_new" element={<PoemPost />} />
           <Route path="/poems" element={<PoemsContainer />} />
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ client/src/PoemPost.js
@@ -0,0 +1,9 @@
+function PoemPost() {
+    return (
+        <div>
+            <form>Post Poem here</form>
+        </div>
+    )
+}
+
+export default PoemPost;
