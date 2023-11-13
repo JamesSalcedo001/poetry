a2f16e67288cd18fdda1a57b76d138f7ea65e560
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-11-13 09:49:38 -0600

    abstracted comments into a comments list component

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,62 @@
+import { Link } from "react-router-dom";
+import { useState } from "react"
 
-function CommentList() {
+
+function CommentList({ loggedIn, comments, setComments, poemId}) {
+
+        const poemComments = comments.filter(comment => comment.poem.id === parseInt(poemId))
+        const [newComment, setNewComment] = useState("")
+
+
+            const handleAddComment = (e) => {
+                e.preventDefault()
+        
+                if(!newComment) return;
+        
+                const commentData = {
+                    content: newComment
+                }
+        
+                fetch(`/poems/${poemId}/comments`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(commentData)
+                })
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error("Failed to post comment")
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    if (data.errors) {
+                        console.error("Error adding comment", data.errors)
+                    } else {
+                        setComments(prevComments => [...prevComments, data])
+                        setNewComment("")
+                    }
+                })
+            }
+
     return (
-        <div>List of comments here for individual poem</div>
+        <div className="comments-container">
+            <ul>
+                {poemComments.map(comment => (
+                    <li key={comment.id}>{comment.content} - <span>Posted by: {comment.user.username}</span></li>
+                ))}
+            </ul>
+            {loggedIn ? (
+                <form onSubmit={handleAddComment}>
+                    <input type="text" value={newComment} onChange={e => setNewComment(e.target.value)} placeholder="Add a comment...">
+                    </input>
+                    <button type="submit">Post comment</button>
+                </form>
+            ) : (
+                <p>You need to <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link> to comment</p>
+            )}
+        </div>
     )
 }
 
--- client/src/PoemDetail.js
+++ client/src/PoemDetail.js
@@ -1,13 +1,12 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { AllContext } from "./context/allContext"
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import CommentList from "./CommentList";
 
 function PoemDetail() {
     const { poems, comments, loggedIn, setComments } = useContext(AllContext)
     const { poemId } = useParams()
     const poem = poems.find(p => p.id === parseInt(poemId))
-    const poemComments = comments.filter(comment => comment.poem.id === parseInt(poemId))
-    const [newComment, setNewComment] = useState("")
 
 
     useEffect(() => {
@@ -15,38 +14,6 @@ function PoemDetail() {
         .then(res => res.json())
         .then(data => setComments(data))
     },[poemId])
-
-    const handleAddComment = (e) => {
-        e.preventDefault()
-
-        if(!newComment) return;
-
-        const commentData = {
-            content: newComment
-        }
-
-        fetch(`/poems/${poemId}/comments`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(commentData)
-        })
-        .then(res => {
-            if (!res.ok) {
-                throw new Error("Failed to post comment")
-            }
-            return res.json()
-        })
-        .then(data => {
-            if (data.errors) {
-                console.error("Error adding comment", data.errors)
-            } else {
-                setComments(prevComments => [...prevComments, data])
-                setNewComment("")
-            }
-        })
-    }
 
 
 
@@ -56,20 +23,12 @@ function PoemDetail() {
             <h2>{poem.title}</h2>
             <p>{poem.content}</p>
             <h5>{poem.created_at}</h5>
-            <ul>
-                {poemComments.map(comment => (
-                    <li key={comment.id}>{comment.content} - <span>Posted by: {comment.user.username}</span></li>
-                ))}
-            </ul>
-            {loggedIn ? (
-                <form onSubmit={handleAddComment}>
-                    <input type="text" value={newComment} onChange={e => setNewComment(e.target.value)} placeholder="Add a comment...">
-                    </input>
-                    <button type="submit">Post comment</button>
-                </form>
-            ) : (
-                <p>You need to <Link to="/login">log in</Link> or <Link to="/signup">sign up</Link> to comment</p>
-            )}
+            <CommentList
+            comments={comments}
+            poemId={poemId}
+            loggedIn={loggedIn}
+            setComments={setComments}
+            />
         </div>
     )
 }
