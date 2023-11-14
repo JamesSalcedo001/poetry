d894ecdad5c016bec4ffdfac6cc86be6f83e14a1
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-11-14 12:44:59 -0600

    changing the color pallette and altering some margins and padding, changed font sizing

--- client/src/PoemPreview.js
+++ client/src/PoemPreview.js
@@ -4,7 +4,7 @@ function PoemPreview({ poem }) {
     return (
         <div className="poem-preview">
             <h3><Link className="poem-links" to={`/poems/${poem.id}`}>{poem.title}</Link></h3>
-            <p>{poem.content ? poem.content.substring(0, 100) + "..." : "Content not available"}</p>
+            <p className="poem-content">{poem.content ? poem.content.substring(0, 100) + "..." : "Content not available"}</p>
         </div>
     )
 }
